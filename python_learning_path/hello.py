5cd5a881abc49c00963a9a0ad84b63f31a7ab228
imjolayemi <njolayemi@gmail.com>
2025-08-24 08:27:47 +0100

    This file shows how to print out Hello World! in python

---
+++ python_learning_path/hello.py
@@ -0,0 +1,10 @@
+print("Hello world!") #code to print hello world
+
+"""
+This is a multi-line comment.
+It can span multiple lines.
+This is useful for documentation or explanations.
+You can also use it to temporarily disable code.
+print("This line will not be executed.")
+
+"""
