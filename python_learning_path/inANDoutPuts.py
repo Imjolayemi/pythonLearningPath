c6938b33fd2485cd38618836d2d69253a7006f9e
imjolayemi <njolayemi@gmail.com>
2025-08-30 08:02:08 +0100

    Docs: documentation on how input and output works in python

---
+++ python_learning_path/inANDoutPuts.py
@@ -0,0 +1,9 @@
+inputANDoutPut="""computer programs can take inputs from users and display outputs"""
+print(inputANDoutPut, "\n")
+# Taking input from users
+takeInput  = input("kindly Enter your name to take input: ")
+print("\nHello", takeInput, "\n")
+
+# Displaying output
+displayOutput = "Welcome to python programming"
+print(displayOutput, "\n")
