e805eb6cf2d3b292d8da4bb14dbdcd90dfcf7f82
imjolayemi <njolayemi@gmail.com>
2025-09-08 07:52:41 +0100

    Doc: Documentation on tuple works in python program

--- python_learning_path/tuples.py
+++ python_learning_path/tuples.py
@@ -26,3 +26,23 @@ print(tupleConstructor, "\n")
 tupleUsingConstructor = tuple(("No", 240, "Agip Area", "Station Road", "Ede", "Osun State", "Nigeria"))
 
 print("This tuple was created using the tuple() constructor \n \t:", tupleUsingConstructor, "\n")
+
+accessingTuple = """ Accessing tuple items can be done using indexing and slicing"""
+print(accessingTuple, "\n")
+print("Accessing the first item in the Tuple below: \n \t", myTuple, " will give us \n \t:", myTuple[0], " \n by refering to the first index number")
+
+print("Accessing the last item in the Tuple below: \n \t", myTuple, " will give us \n \t:", myTuple[-1], " \n by refering to the last index number using negative indexing \n ")
+
+rangeOfItems = """You can also access a range of items in a tuple by using the slicing syntax"""
+print(rangeOfItems, "\n")
+
+print("Accessing items from index 1 to index 4 in the Tuple below: \n \t", myTuple, " will give us \n \t:", myTuple[1:5], "\n by refering to the index numbers in the slicing syntax \n ")
+
+print("Accessing items from the index 2 to the end in the Tuple below: \n \t", myTuple, " will give us \n \t:", myTuple[2:], "\n by refering to the starting index number in the slicing syntax \n ")
+
+
+checkingItem = """You can check if an item exists in a tuple by using the 'in' keyword"""
+print(checkingItem, "\n")
+
+if "Akure" in myTuple:
+    print("This output is from checking if 'Akure' exists in the Tuple below \n \t:", myTuple, "\n \t: Yes, 'Akure' is in the tuple \n ")
